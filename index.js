f47b32457f0e3ed3c4128664b42440513a26cd0c
Choirul Rahmaditya <59856535+Choi-R@users.noreply.github.com>
2021-10-10 20:28:41 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-let array_nama = ["nama1", "nama2", "namaseterusnya", "dst"]
-console.log(array_nama)
+let gelobal = ["nama1", "nama2", "namaseterusnya", "dst"] 
+
+// Kode di bawah untuk dibaca saja, jangan diubah ya. Pengerjaan kalian cukup di atas ini
+let sebuah_fungsi = function(suatu_input) {
+  console.log("ini variabel global ->", "gelobal =", gelobal)
+  let lokal = {Plis: "nonton", anime: "Clannad"}
+  console.log("ini variabel lokal ->", "lokal =", lokal)
+  console.log("ini variabel lokal juga nggak sih? ->", "suatu_input =", suatu_input)
+  return "Semangat belajarnya, sukses selalu!"
+  }
+
+let hasil_eksekusi = sebuah_fungsi (gelobal)
+console.log(hasil_eksekusi)
