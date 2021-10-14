da3f254874c37d98909abe8abbd15886e1755dd2
Fajar Kresna <f.kresna23@gmail.com>
2021-10-14 09:47:40 +0700

    [Fajar Kresna Pahlawan] sorry kak, kemarin salah tangkap

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-let gelobal = ["nama1", "nama2", "namaseterusnya", "dst"] 
+let gelobal = ["nama1", "nama2", "namaseterusnya", "dst"];
 
-gelobal = ["Mochamad Dena Eka Putra", "nama2", "namaseterusnya", "dst"]
+gelobal = [
+  "Mochamad Dena Eka Putra",
+  "Fajar Kresna Pahlawan",
+  "namaseterusnya",
+  "dst",
+];
 
 // Kode di bawah untuk dibaca saja, jangan diubah ya. Pengerjaan kalian cukup di atas ini
-let sebuah_fungsi = function(suatu_input) {
-  console.log("ini variabel global ->", "gelobal =", gelobal)
-  let lokal = {Plis: "nonton", anime: "Clannad"}
-  console.log("ini variabel lokal ->", "lokal =", lokal)
-  console.log("ini variabel lokal juga nggak sih? ->", "suatu_input =", suatu_input)
-  return "Semangat belajarnya, sukses selalu!"
-  }
+let sebuah_fungsi = function (suatu_input) {
+  console.log("ini variabel global ->", "gelobal =", gelobal);
+  let lokal = { Plis: "nonton", anime: "Clannad" };
+  console.log("ini variabel lokal ->", "lokal =", lokal);
+  console.log(
+    "ini variabel lokal juga nggak sih? ->",
+    "suatu_input =",
+    suatu_input
+  );
+  return "Semangat belajarnya, sukses selalu!";
+};
 
-let hasil_eksekusi = sebuah_fungsi (gelobal)
-console.log(hasil_eksekusi)
+let hasil_eksekusi = sebuah_fungsi(gelobal);
+console.log(hasil_eksekusi);
