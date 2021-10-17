541e1124717ff85833bd6aecac2c9a9e9a4483da
Choirul Rahmaditya <59856535+Choi-R@users.noreply.github.com>
2021-10-18 07:53:41 +0800

    Update w2.js

--- w2.js
+++ w2.js
@@ -1,3 +1,5 @@
+// Silahkan teman-teman membuat branch sendiri menggunakan nama lengkap (jika belum), kemudian mengerjakan soal berikut di branch masing-masing
+
 const nomorSatu =`Diberikan sebuah variabel bernama 'arr' berisi array. 
     Dengan hanya menggunakan metode .push, .pop, .shift dan .unshift, jadikanlah array tersebut berisi angka urut dari 1 sampai 9!`
 const arr = [-6, 9999, 3, 4, 5, 6, 7, 6, 5]
@@ -48,3 +50,4 @@ const nomorTiga = `uhh... ngoding aja apapun, yang penting ada "if else"nya. Tid
 // Silahkan mengerjakan soal nomorTiga di atas ini (jumlah baris yang dibutuhkan mungkin berbeda, sesuaikan saja)
 // NB: Sepertinya soal nomorTiga berpotensi tinggi untuk copas kodingan dari tempat lain karena konteksnya bebas, tapi plis bikin sendiri. Selamat mengerjakan
 // NB2: Oh iya, pastikan setelah selesai semua, filenya tidak ada error apapun saat dijalankan ya ("node w2.js" di terminal)
+// NB3: Oh iya lagi, boleh membuat file tambahan tetapi soalnya tetap dikerjakan di file ini dan jawaban utamanya tetap berada di tempat yang disediakan
