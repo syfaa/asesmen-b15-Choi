9a49170bed11ea6bb693be3443cc37d13ca99630
Anggun Wahyuni MZ <43981095+anggunwahyunimz@users.noreply.github.com>
2021-10-21 23:02:11 +0800

    Weekly Assesment week 2 Anggun

--- w2.js
+++ w2.js
@@ -1,53 +1,40 @@
-// Silahkan teman-teman membuat branch sendiri menggunakan nama lengkap (jika belum), kemudian mengerjakan soal berikut di branch masing-masing
+/*Nomor 1 */
+const arr = [-6, 9999, 3, 4, 5, 6, 7, 6, 5];
+arr.shift();
+arr.shift();
+arr.unshift(1, 2);
+arr.pop();
+arr.pop();
+arr.push(8, 9);
+console.log(arr);
 
-const nomorSatu =`Diberikan sebuah variabel bernama 'arr' berisi array. 
-    Dengan hanya menggunakan metode .push, .pop, .shift dan .unshift, jadikanlah array tersebut berisi angka urut dari 1 sampai 9!`
-const arr = [-6, 9999, 3, 4, 5, 6, 7, 6, 5]
-// contoh: arr.push(9); arr.pop(); arr.pop(); arr.unshift(2); dst....
-// hasil akhir arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-// Silahkan mengerjakan di bawah ini (jumlah baris yang dibutuhkan mungkin berbeda, sesuaikan saja)
-
-
-
-
-
-// Silahkan mengerjakan soal nomorSatu di atas ini (jumlah baris yang dibutuhkan mungkin berbeda, sesuaikan saja)
-
-
-
-const nomorDua = `Diberikan sebuah variabel bernama 'gelobal' berisi array. 
-    Ubahlah setiap elemen di dalam array tersebut dari string menjadi objek yang berisi string aslinya utuh dan huruf pertama dari string aslinya. 
-    Silahkan menggunakan satu dari tiga cara berikut:
-    1. looping (for atau while)
-    2. .map
-    3. .forEach`
-const gelobal = ["Mochamad Dena Eka Putra", "Fajar Kresna Pahlawan", "Gusti Arsyad", "Nanda Nipri D", "Fadhitya Syafa Andini", "Hardiansyah Rochani", "Kevin Marchiano", "Anggun Wahyuni", "Khairul Umam", "Fidoni Agusta Hartoyo"];
-// contoh hasil = [{asli: "Choirul R", hurufPertama: "c"}, {asli: "Clannad After Story", hurufPertama: "C"}]
-// Silahkan mengerjakan soal nomorDua di bawah ini (jumlah baris yang dibutuhkan mungkin berbeda, sesuaikan saja)
+/*Nomor 2*/
+const gelobal = ["Mochamad Dena Eka Putra", "Fajar Kresna Pahlawan", "Gusti Arsyad", "Nanda Nipri D", "Fadhitya Syafa Andini", "Hardiansyah Rochani", "Kevin Marchiano", "Anggun Wahyuni", "Khairul Umam", "Fidoni Agusta Hartoyo"];  
+let objectGelobal= [];
+objectGelobal = gelobal.map(function (objectString) {
+    let hasil = {};
+    hasil["asli"] = objectString;
+    hasil["hurufPertama"] = objectString.charAt(0);
+    return hasil;
+});
+console.log(objectGelobal);
 
 
+/*Nomor 3*/
+let nyatakanCintaPadaAnggun = true;
+let kepastianCintaPadaAnggun = true;
+let menerimaAnggunApaAdanya = true;
+let andaKaya = true;
+let andaGoodLooking = false;
 
-
-
-// Silahkan mengerjakan soal nomorDua di atas ini (jumlah baris yang dibutuhkan mungkin berbeda, sesuaikan saja)
-
-
-
-
-
-const nomorTiga = `uhh... ngoding aja apapun, yang penting ada "if else"nya. Tidak boleh ada yang mirip ya dengan contoh maupun dengan teman yang lainnya. Indikator mirip nggaknya ya nanti kita lihat`
-// contoh :
-// let udahNontonClannad = true
-// if (udahNontonClannad) { console.log("nice") }
-// else { console.log("buruan nonton") }
-// contoh selesai
-// Silahkan mengerjakan soal nomorTiga di bawah ini (jumlah baris yang dibutuhkan mungkin berbeda, sesuaikan saja)
-
-
-
-
-
-// Silahkan mengerjakan soal nomorTiga di atas ini (jumlah baris yang dibutuhkan mungkin berbeda, sesuaikan saja)
-// NB: Sepertinya soal nomorTiga berpotensi tinggi untuk copas kodingan dari tempat lain karena konteksnya bebas, tapi plis bikin sendiri. Selamat mengerjakan
-// NB2: Oh iya, pastikan setelah selesai semua, filenya tidak ada error apapun saat dijalankan ya ("node w2.js" di terminal)
-// NB3: Oh iya lagi, boleh membuat file tambahan tetapi soalnya tetap dikerjakan di file ini dan jawaban utamanya tetap berada di tempat yang disediakan
+if (nyatakanCintaPadaAnggun && kepastianCintaPadaAnggun && menerimaAnggunApaAdanya){
+    console.log('Anggun I love you');
+    console.log('Will you marry me?');
+    console.log('Aku terima kamu apa adanya...');
+}else if(nyatakanCintaPadaAnggun && andaGoodLooking && andaKaya){
+    console.log('Anggun I love you');
+    console.log('Aku ganteng loh!');
+    console.log('Aku kaya lagi!')
+}else{
+    console.log('Maap ya Anggun, aku udah punya pacar:)');
+}
